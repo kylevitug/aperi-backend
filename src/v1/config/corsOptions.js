98e41a6e976b7614321f86e586148e80f081f2d0
Kyle Vitug <kyle_vitug@yahoo.com.ph>
2024-10-17 16:02:46 +0800

    cors allow all

--- src/v1/config/corsOptions.js
+++ src/v1/config/corsOptions.js
@@ -1,14 +1,20 @@
 const allowedOrigins = require('./allowedOrigins');
 
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     optionsSuccessStatus: 200
+// }
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+      callback(null, true); // Allow all origins
     },
-    optionsSuccessStatus: 200
-}
+    credentials: true, // Allow cookies or credentials
+  };
 
 module.exports = corsOptions;
