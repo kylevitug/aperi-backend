3e541e849e93bcbdca32c4cf7bb08986d7a84323
Kyle Vitug <kyle_vitug@yahoo.com.ph>
2024-10-24 23:10:20 +0800

    Update services.js

--- src/v2/api/purchases/services.js
+++ src/v2/api/purchases/services.js
@@ -62,7 +62,80 @@ const getLastPurchaseByPrincipalNameByServerId = async (serverId, principalName)
   }
 }
 
+const getAllPurchaseHeaders = async (filters = {}) => {
+  try {
+    let sql = `SELECT * FROM purchaseheader WHERE 1=1`; // Base query
+    let values = [];
+
+    // Dynamically add filters based on the fields provided in the request
+    if (filters.purchaseheaderid) {
+      sql += ` AND purchaseheaderid = ?`;
+      values.push(filters.purchaseheaderid);
+    }
+    if (filters.purchasedate) {
+      sql += ` AND purchasedate = ?`;
+      values.push(filters.purchasedate);
+    }
+    if (filters.supplierid) {
+      sql += ` AND supplierid = ?`;
+      values.push(filters.supplierid);
+    }
+    if (filters.refnumber) {
+      sql += ` AND refnumber = ?`;
+      values.push(filters.refnumber);
+    }
+    if (filters.memo) {
+      sql += ` AND memo = ?`;
+      values.push(filters.memo);
+    }
+    if (filters.total) {
+      sql += ` AND total = ?`;
+      values.push(filters.total);
+    }
+    if (filters.employeeid) {
+      sql += ` AND employeeid = ?`;
+      values.push(filters.employeeid);
+    }
+    if (filters.isreceived) {
+      sql += ` AND isreceived = ?`;
+      values.push(filters.isreceived);
+    }
+    if (filters.datecreated) {
+      sql += ` AND datecreated = ?`;
+      values.push(filters.datecreated);
+    }
+    if (filters.datemodified) {
+      sql += ` AND datemodified = ?`;
+      values.push(filters.datemodified);
+    }
+    if (filters.deliverydate) {
+      sql += ` AND deliverydate = ?`;
+      values.push(filters.deliverydate);
+    }
+    if (filters.supplierclassid) {
+      sql += ` AND supplierclassid = ?`;
+      values.push(filters.supplierclassid);
+    }
+    if (filters.billedstatus) {
+      sql += ` AND billedstatus = ?`;
+      values.push(filters.billedstatus);
+    }
+
+    // Execute the query
+    const db = await connectMasterDb();
+    const [rows] = await db.execute(sql, values);
+    await db.end();
+
+    return rows;
+  } catch (error) {
+    console.error('Error ', error);
+    throw error;
+  }
+};
+
+
 module.exports = {
   getLastThreeByPrincipalName,
-  getLastPurchaseByPrincipalNameByServerId
+  getLastPurchaseByPrincipalNameByServerId,
+  getAllPurchaseHeaders
 };
