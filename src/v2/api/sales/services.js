1acece06f235225fa990e085daf0c41851afd765
Kyle Vitug <kyle_vitug@yahoo.com.ph>
2024-11-19 14:36:42 +0800

    save

--- src/v2/api/sales/services.js
+++ src/v2/api/sales/services.js
@@ -25,6 +25,32 @@ const getAllSales = async (req, res) => {
   }
 };
 
+const getAllSalesByProductGroupNameByDateByServerId = async (req, res) => {
+  let { productgroupid, serverid, startdate, enddate } = req.body;
+  try {
+    const sql = `
+      SELECT
+        DATE(saleheader.datecreated) AS datecreated,
+        SUM(saleline.total) AS total
+      FROM saleheader, saleline, item
+      WHERE saleheader.saleheaderid = saleline.saleheaderid
+      AND saleline.itemid = item.itemid
+      AND item.productgroupid = ?
+      AND DATE(saleheader.datecreated) BETWEEN ? AND ?
+      GROUP BY DATE(saleheader.datecreated)`;
+    const db = await connectDbByServerId(serverid);
+    const [rows, fields] = await db.execute(sql, [
+      productgroupid,
+      startdate,
+      enddate,
+    ]);
+    await db.end();
+    return rows;
+  } catch (error) {
+    console.error('Error ', error);
+  }
+}
+
 module.exports = {
   getAllSales,
 };
